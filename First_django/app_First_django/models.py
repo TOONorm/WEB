fdf902a9246327dabc98cb77d963d17ca8f6f615
TOONorm <segamegadraiv200@ya.ru>
2023-08-02 09:53:38 +0500

    ДЗ занятие 7

--- First_django/app_First_django/models.py
+++ First_django/app_First_django/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Advert(models.Model):
+    ID = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    title = models.CharField('Название', max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=3)
+    auction = models.BooleanField('Торг', help_text='Укажите, возможен ли торг')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return (f"{self.app_label}(id={self.ID}, title={self.title}, price-{self.price}")
